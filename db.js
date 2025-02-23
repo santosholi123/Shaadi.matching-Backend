ed4c009abdb9850eaec0c662a6762d9f3bf99c71
santosholi123 <152282143+santosholi123@users.noreply.github.com>
2025-02-23 13:04:24 +0545

    Add files via upload

--- db.js
+++ db.js
@@ -1,23 +1,9 @@
-const pgp = require('pg-promise')();
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
 
-// Connect to PostgreSQL using environment variables
-const db = pgp({
-    host: process.env.PG_HOST,        // Host (e.g., localhost)
-    port: process.env.PG_PORT,        // Port number (default is 5432)
-    database: process.env.PG_DATABASE, // Database name (e.g., register)
-    user: process.env.PG_USER,        // PostgreSQL username (e.g., postgres)
-    password: process.env.PG_PASSWORD // PostgreSQL password (e.g., 230558)
+const sequelize = new Sequelize("register", "postgres", "230558", {
+    host: "localhost",
+    dialect: "postgres",
+    logging: false, // Disable query logging
 });
 
-// Test the connection
-db.connect()
-    .then((obj) => {
-        console.log('Connected to PostgreSQL:', obj.client.database);
-        obj.done(); // Release the connection
-    })
-    .catch((error) => {
-        console.error('Error connecting to PostgreSQL:', error.message || error);
-    });
-
-module.exports = db;
+module.exports = sequelize;
